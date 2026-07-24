d21e16aad1823a2dad069a1f6116afbc57284a9c
Tej Praval PULA <143714168+Tejpraval@users.noreply.github.com>
2026-07-24 10:09:52 +0530

    Add HelloWorld class with multiple print statements

---
+++ helloword.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World dfghjkjhgfdsa!");
+        System.out.println("Hello, Kya haal chall h jenkins Poll Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm one more");
+    }
+}
