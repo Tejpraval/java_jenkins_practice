7ac1d56e57bf85aa77c8651292b7d61a1f2b5309
Tej Praval PULA <143714168+Tejpraval@users.noreply.github.com>
2026-07-27 09:43:30 +0530

    Rename class from simcalculator to simpleCalculator

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class simcalculator {
+public class simpleCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a:");
